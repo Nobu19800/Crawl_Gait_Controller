c8e6595223d983987af844395854204b706fda3c
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2017-03-08 13:30:10 +0900

    1330

--- include/Crawl_Gait_Controller/Crawl_Gait_Controller.h
+++ include/Crawl_Gait_Controller/Crawl_Gait_Controller.h
@@ -368,12 +368,12 @@ class Crawl_Gait_Controller
 
 	 // DataOutPort declaration
 	 // <rtc-template block="outport_declare">
-	 RTC::LeggedRobot m_Trajectory;
+	 WalkingRobot::LeggedRobot m_Trajectory;
 	 /*!
 	  * 足先軌道
 	  * - Type: RTC::LeggedRobot
 	  */
-	 OutPort<RTC::LeggedRobot> m_TrajectoryOut;
+	 OutPort<WalkingRobot::LeggedRobot> m_TrajectoryOut;
 	 RTC::TimedPose2D m_current_pose;
 	 /*!
 	 * 現在位置
@@ -398,13 +398,34 @@ class Crawl_Gait_Controller
   
   // </rtc-template>
 
-  // CORBA Port declaration
-  // <rtc-template block="corbaport_declare">
-  
-  // </rtc-template>
+	 // CORBA Port declaration
+	 // <rtc-template block="corbaport_declare">
+	 /*!
+	 * サーボ操作コマンド
+	 */
+	 RTC::CorbaPort m_LeggedRobotCommonInterface_ServoPort;
+	 /*!
+	 * ロボット操作コマンド
+	 */
+	 RTC::CorbaPort m_LeggedRobotCommonInterface_RobotPort;
 
-  // Service declaration
-  // <rtc-template block="service_declare">
+	 // </rtc-template>
+
+	 // Service declaration
+	 // <rtc-template block="service_declare">
+
+	 // </rtc-template>
+
+	 // Consumer declaration
+	 // <rtc-template block="consumer_declare">
+	 /*!
+	 * サーボ操作コマンド
+	 */
+	 RTC::CorbaConsumer<WalkingRobot::LeggedRobotCommonInterface_Servo> m_LeggedRobotCommonInterface_Servo;
+	 /*!
+	 * ロボット操作コマンド
+	 */
+	 RTC::CorbaConsumer<WalkingRobot::LeggedRobotCommonInterface_Robot> m_LeggedRobotCommonInterface_Robot;
   
   // </rtc-template>
 
